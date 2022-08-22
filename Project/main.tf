ec6ae679c19c1d8f8f516f11d4ca193daebadcfb
teslaluv <109180406+teslaluv@users.noreply.github.com>
2022-08-23 02:14:19 +0300

    Update main.tf

--- Project/main.tf
+++ Project/main.tf
@@ -2,35 +2,6 @@ provider "aws" {
   region = var.region
 }
 
-# module "vpc-default" {
-#   source = "../modules/aws_network"
-# }
-
-# module "vpc-dev" {
-#   source = "../modules/network"
-#   env                  = "dev"
-#   vpc_cidr             = "10.100.0.0/16"
-#   public_subnet_cidrs  = ["10.100.1.0/24", "10.100.2.0/24"]
-#   private_subnet_cidrs = []
-# }
-
-# module "vpc-prod" {
-#   source = "../modules/aws_network"
-#   // source               = "git@github.com:adv4000/terraform-modules.git//aws_network"
-#   env                  = "prod"
-#   vpc_cidr             = "10.10.0.0/16"
-#   public_subnet_cidrs  = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
-#   private_subnet_cidrs = ["10.10.11.0/24", "10.10.22.0/24", "10.10.33.0/24"]
-# }
-
-# module "vpc-test" {
-#   source = "../modules/aws_network"
-#   env                  = "staging"
-#   vpc_cidr             = "10.10.0.0/16"
-#   public_subnet_cidrs  = ["10.10.1.0/24", "10.10.2.0/24"]
-#   private_subnet_cidrs = ["10.10.11.0/24", "10.10.22.0/24"]
-# }
-
 module "vpc-dev" {
   source = "../modules/cluster"
   env                  = "dev"
@@ -43,7 +14,3 @@ module "vpc-dev" {
 module "ecr" {
   source = "../modules/ecr"
 }
-
-# module "cluster" {
-#   source = "../modules/cluster"
-# }
